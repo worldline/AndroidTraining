3c0018c6a5672c0cc198e569c071cfd8031ad01b
ffacon <francois.facon@gmail.com>
2015-09-16 11:18:33 +0200

    add OnClickListener interface

--- WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterLoginActivity.java
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterLoginActivity.java
@@ -1,14 +1,22 @@
 package worldline.ssm.rd.ux.wltwitter;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
-public class WLTwitterLoginActivity extends Activity {
+public class WLTwitterLoginActivity extends Activity
+        implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
 
