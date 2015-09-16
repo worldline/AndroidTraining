95b30d5f58c411152a50f7260628202469325622
ffacon <francois.facon@gmail.com>
2015-09-16 10:59:06 +0200

    Create the WLTwitterLoginActivity class that will be on charge of  the activity_login layout

---
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterLoginActivity.java
@@ -0,0 +1,15 @@
+package worldline.ssm.rd.ux.wltwitter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WLTwitterLoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
+
