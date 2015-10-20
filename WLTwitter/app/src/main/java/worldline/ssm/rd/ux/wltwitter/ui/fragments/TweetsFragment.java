045884cfdbeb30e6c785ad925d82c8135c2660f6
ffacon <francois.facon@gmail.com>
2015-10-20 12:04:25 +0200

    Start AsyncTask in TweetsFragment

--- WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/ui/fragments/TweetsFragment.java
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/ui/fragments/TweetsFragment.java
@@ -3,17 +3,22 @@ package worldline.ssm.rd.ux.wltwitter.ui.fragments;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import worldline.ssm.rd.ux.wltwitter.R;
+import worldline.ssm.rd.ux.wltwitter.async.RetrieveTweetsAsyncTask;
+import worldline.ssm.rd.ux.wltwitter.utils.PreferenceUtils;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TweetsFragment extends Fragment {
 
+    // Keep a reference to the AsyncTask
+    private RetrieveTweetsAsyncTask mTweetAsyncTask;
 
     public TweetsFragment() {
         // Required empty public constructor
@@ -28,4 +33,15 @@ public class TweetsFragment extends Fragment {
     }
 
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // On start launch our AsyncTask to retrieve the Tweets of the user
+        final String login = PreferenceUtils.getLogin();
+        if (!TextUtils.isEmpty(login)){
+            mTweetAsyncTask = new RetrieveTweetsAsyncTask();
+            mTweetAsyncTask.execute(login);
+        }
+    }
 }
