b71ba579a450b00e9b2dfa7e4c5bfc218766b002
ffacon <francois.facon@gmail.com>
2015-10-20 11:11:12 +0200

    Add new TweetsFragment class

---
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/ui/fragments/TweetsFragment.java
@@ -0,0 +1,31 @@
+package worldline.ssm.rd.ux.wltwitter.ui.fragments;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import worldline.ssm.rd.ux.wltwitter.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TweetsFragment extends Fragment {
+
+
+    public TweetsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wltwitter, container, false);
+    }
+
+
+}
