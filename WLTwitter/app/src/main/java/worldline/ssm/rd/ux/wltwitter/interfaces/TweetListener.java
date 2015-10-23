569b1ce0df199029e04f29a1cc0754f9218d4571
ffacon <francois.facon@gmail.com>
2015-10-23 16:35:21 +0200

    Add TweetListenerInterface

---
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/interfaces/TweetListener.java
@@ -0,0 +1,10 @@
+package worldline.ssm.rd.ux.wltwitter.interfaces;
+
+import worldline.ssm.rd.ux.wltwitter.pojo.Tweet;
+
+public interface TweetListener {
+
+	public void onRetweet(Tweet tweet);
+	public void onViewTweet(Tweet tweet);
+	
+}
