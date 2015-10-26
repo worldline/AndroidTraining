6cf607c9bcd1ff2f3e5605ce7a912a35a70e9244
ffacon <francois.facon@gmail.com>
2015-10-26 13:52:10 +0100

    LayoutInflater to load a View from XML layout

--- WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/adapters/TweetsAdapter.java
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/adapters/TweetsAdapter.java
@@ -1,12 +1,14 @@
 package worldline.ssm.rd.ux.wltwitter.adapters;
 
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 import java.util.List;
 
+import worldline.ssm.rd.ux.wltwitter.WLTwitterApplication;
 import worldline.ssm.rd.ux.wltwitter.pojo.Tweet;
 
 public class TweetsAdapter extends BaseAdapter {
@@ -14,8 +16,12 @@ public class TweetsAdapter extends BaseAdapter {
     // The list of items to display
     private final List<Tweet> mTweets;
 
+    // The Layout inflater
+    private final LayoutInflater mInflater;
+
     public TweetsAdapter(List<Tweet> tweets) {
         mTweets = tweets;
+        mInflater = LayoutInflater.from(WLTwitterApplication.getContext());
     }
 
     @Override
