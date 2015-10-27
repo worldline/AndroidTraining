a15c1f374a2933185cfe9d4194cda106bf389b6d
ffacon <francois.facon@gmail.com>
2015-10-27 14:33:12 +0100

    Implement  Adapter interface

--- WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/adapters/TweetsAdapter.java
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/adapters/TweetsAdapter.java
@@ -5,9 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
+import worldline.ssm.rd.ux.wltwitter.R;
 import worldline.ssm.rd.ux.wltwitter.WLTwitterApplication;
 import worldline.ssm.rd.ux.wltwitter.pojo.Tweet;
 
@@ -26,21 +28,34 @@ public class TweetsAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return null != mTweets ? mTweets.size() : 0;
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return null != mTweets ? mTweets.get(position) : null;
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        // Inflate our View
+        final View view = mInflater.inflate(R.layout.tweet_listitem, null);
+        // Get the current item
+        final Tweet tweet = (Tweet) getItem(position);
+        // Set the user name, to do so, retrieve the corresponding TextView
+        final TextView userName = (TextView) view.findViewById(R.id.tweetListItemNameTextView);
+        userName.setText(tweet.user.name);
+        // Set the user alias, to do so, retrieve the corresponding TextView
+        final TextView userAlias = (TextView) view.findViewById(R.id.tweetListItemAliasTextView);
+        userAlias.setText("@" + tweet.user.screenName);
+        // Set the text of the tweet, to do so, retrieve the corresponding TextView
+        final TextView text = (TextView) view.findViewById(R.id.tweetListItemTweetTextView);
+        text.setText(tweet.text);
+        return view;
     }
 }
