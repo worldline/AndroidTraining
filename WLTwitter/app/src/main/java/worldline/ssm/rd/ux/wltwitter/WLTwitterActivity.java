dfcf338224e70b5c0244139f2a3b41d92ab8bcca
ffacon <francois.facon@gmail.com>
2015-10-30 21:02:02 +0100

    Use Activity to display TweetFragment on onViewTweet Callback

--- WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterActivity.java
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterActivity.java
@@ -1,6 +1,7 @@
 package worldline.ssm.rd.ux.wltwitter;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 import worldline.ssm.rd.ux.wltwitter.async.RetrieveTweetsAsyncTask;
 import worldline.ssm.rd.ux.wltwitter.interfaces.TweetListener;
 import worldline.ssm.rd.ux.wltwitter.pojo.Tweet;
+import worldline.ssm.rd.ux.wltwitter.ui.fragments.TweetFragment;
 import worldline.ssm.rd.ux.wltwitter.ui.fragments.TweetsFragment;
 import worldline.ssm.rd.ux.wltwitter.utils.Constants;
 import worldline.ssm.rd.ux.wltwitter.utils.PreferenceUtils;
@@ -80,7 +82,19 @@ public class WLTwitterActivity extends Activity implements TweetListener {
 
     @Override
     public void onViewTweet(Tweet tweet) {
-        Toast.makeText(this, tweet.text, Toast.LENGTH_LONG).show();
+        final FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+        final TweetFragment fragment = new TweetFragment();
+        final Bundle bundle = new Bundle();
+        bundle.putString(Constants.Tweet.EXTRA_TEXT, tweet.text);
+        bundle.putString(Constants.Tweet.EXTRA_IMAGEURL, tweet.user.profileImageUrl);
+        bundle.putString(Constants.Tweet.EXTRA_NAME, tweet.user.name);
+        bundle.putString(Constants.Tweet.EXTRA_ALIAS, tweet.user.screenName);
+        fragment.setArguments(bundle);
+
+        transaction.add(R.id.container, fragment);
+        transaction.setCustomAnimations(R.animator.slide_in_right, 0, 0, R.animator.slide_out_left);
+        transaction.addToBackStack(null).commit();
     }
 
 }
