5a7431681e8560a647b8e74e491a59ed5af41fd6
ffacon <francois.facon@gmail.com>
2015-10-31 23:06:39 +0100

    populate tweetFragment

--- WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/ui/fragments/TweetFragment.java
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/ui/fragments/TweetFragment.java
@@ -6,8 +6,10 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import worldline.ssm.rd.ux.wltwitter.R;
+import worldline.ssm.rd.ux.wltwitter.utils.Constants;
 
 public class TweetFragment extends Fragment {
 
@@ -21,7 +23,21 @@ public class TweetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wltwitter_tweet, container, false);
+        final View view = inflater.inflate(R.layout.fragment_wltwitter_tweet, container, false);
+
+        // Set the name
+        final String name = getArguments().getString(Constants.Tweet.EXTRA_NAME);
+        ((TextView) view.findViewById(R.id.tweetNameTextView)).setText(name);
+
+        // Set the alias
+        final String alias = getArguments().getString(Constants.Tweet.EXTRA_ALIAS);
+        ((TextView) view.findViewById(R.id.tweetAliasTextView)).setText("@" + alias);
+
+        // Set the text
+        final String text = getArguments().getString(Constants.Tweet.EXTRA_TEXT);
+        ((TextView) view.findViewById(R.id.tweetTextTextView)).setText(text);
+
+        return view;
     }
 
 
