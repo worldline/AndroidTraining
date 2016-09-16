e823846f06737406f0c7852eb6e79fe8265e7041
ffacon <francois.facon@gmail.com>
2016-09-16 16:10:36 +0200

    Execute AsyncTask

---
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/async/RetrieveTweetsAsyncTask.java
@@ -0,0 +1,32 @@
+package worldline.ssm.rd.ux.wltwitter.async;
+
+import java.util.List;
+
+import worldline.ssm.rd.ux.wltwitter.WLTwitterActivity;
+import worldline.ssm.rd.ux.wltwitter.helpers.TwitterHelper;
+import worldline.ssm.rd.ux.wltwitter.pojo.Tweet;
+import android.os.AsyncTask;
+
+public class RetrieveTweetsAsyncTask extends AsyncTask<String, Void, List<Tweet>>{
+
+
+	public RetrieveTweetsAsyncTask(){
+	}
+	
+	@Override
+	protected List<Tweet> doInBackground(String... params) {
+		if ((null != params) && (params.length > 0)){
+			return TwitterHelper.getTweetsOfUser(params[0]);
+		}
+		return null;
+	}
+
+	@Override
+	protected void onPostExecute(List<Tweet> result) {
+		if (null != result){
+            for (int i=0;i<result.size();i++)
+                 System.out.println(WLTwitterActivity.class.getName()+ result.get(i).text);
+		}
+	}
+	
+}
