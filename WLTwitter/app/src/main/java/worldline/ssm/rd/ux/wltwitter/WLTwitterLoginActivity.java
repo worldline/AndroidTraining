dfa7230acd051d20623c54ffb0fde694f85c98e8
ffacon <francois.facon@gmail.com>
2018-06-03 16:00:59 +0200

    Step 1: add Login layout & activity

---
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterLoginActivity.java
@@ -0,0 +1,15 @@
+package worldline.ssm.rd.ux.wltwitter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+public class WLTwitterLoginActivity extends Activity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
